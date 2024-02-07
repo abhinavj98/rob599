5e077e1fa5f56bb2db9cb4f2fc27e8452f921545
Abhinav Jain <abhinav98jain@gmail.com>
2024-02-07 12:17:32 -0800

    mbook changes

--- src/rob599/rob599_hw1/src/hw1_3.py
+++ src/rob599/rob599_hw1/src/hw1_3.py
@@ -1,44 +1,64 @@
 #!/usr/bin/env python
-
-# A more encapsulated version of navigation for the Fetch.
-#
-# better_nav.py
-#
-# Bill Smart
-#
-# This example shows a more encapsulated version of basic navigation for the Fetch.  The functionality
-# is the same as basic_nav.py, but the interface presented to the user is simpler, and specialized to
-# the Fetch.
-
-
 import rospy
-import actionlib
-
-# We need the MoveBaseAction and MoveBaseGoal from the move_base_msgs package.
-from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
-
-# We're going to use the quaternion type here.
 from geometry_msgs.msg import Quaternion, Twist
-
-# tf includes a handy set of transformations to move between Euler angles and quaternions (and back).
-from tf import transformations
-
-import sys
 from sensor_msgs.msg import LaserScan
+from typing import List
+import numpy as np
+
+
+class VelocityController:
+    """Velocity controller for the fetch robot"""
+    def __init__(self):
+        self.vel_publisher = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+
+    def publish_vel(self, vel: List[float, float]):
+        msg = Twist()
+        msg.linear.x = vel[0]
+        msg.angular.z = vel[1]
+
+        self.vel_publisher.publish(msg)
+
+    def stop(self):
+        self.publish_vel([0, 0])
+
+    def forward(self, ranges: List[float]):
+        if min(ranges) < 1:
+            self.stop()
+        else:
+            min_dist = np.min(ranges)
+            velocity = 0.5*(min_dist - 1)
+            #clip velocity to 0 and 0.5
+            velocity = max(0, min(0.5, velocity))
+            self.publish_vel([velocity, 0])
 
 class FetchMove:
     """Move fetch with velocity commands"""
     def __init__(self):
-        self.vel_publisher = rospy.Publisher('cmd_vel', Twist, queue_size=10)
-        self.laser_scan_subscriber = rospy.Subscriber('base_scan', LaserScan, laser_scan_callback)
+        self.laser_scan_subscriber = rospy.Subscriber('base_scan', LaserScan, self.laser_scan_callback)
+        self.vel_controller = VelocityController()
+        self.size = 1
     
-    def publish_vel(self):
-        msg = Twist()
-        msg.linear.x = 0.1
-        self.vel_publisher.publish(msg)
-
     def laser_scan_callback(self, msg):
-        print(msg)
+        ranges_cartesian = self.polar_to_cartesian(msg.ranges, msg.angle_min, msg.angle_max, msg.angle_increment)
+
+
+    def polar_to_cartesian(self, ranges: List[float], angle_min: float, angle_max: float, angle_increment: float):
+        angle = np.arange(angle_min, angle_max, angle_increment)
+        x = ranges * np.cos(angle)
+        y = ranges * np.sin(angle)
+        return x, y
+
+    def is_point_in_front(self, range: float):
+        fetch_points = [0.5, 0.5]#get transform from world to laser
+        #Will need robot orientation to get the correct min and max
+
+        x_min = fetch_points[0] - 0.5
+        x_max = fetch_points[0] + 0.5
+        y_min = fetch_points[1]
+        y_max = fetch_points[1] + 1
+        x = range[0]
+        y = range[1]
+        return x_min < x < x_max and y_min < y < y_max
 
 
 
