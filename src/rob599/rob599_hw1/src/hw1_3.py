289b54f39d773388bd306a37075cd33c3732a323
Abhinav <abhinav98jain@gmail.com>
2024-02-07 13:47:00 -0800

    wrote laser scan filter not tested

--- src/rob599/rob599_hw1/src/hw1_3.py
+++ src/rob599/rob599_hw1/src/hw1_3.py
@@ -32,57 +32,18 @@ class VelocityController:
             velocity = max(0, min(1, velocity))
             self.publish_vel([velocity, 0])
     
-    def forward_cartesian(self, ranges):
-        if ranges is None:
-            self.stop()
-            return
-        dist = np.linalg.norm(ranges)
-        if min(ranges) < 1:
-            self.stop()
-        else:
-            min_dist = np.min(dist)
-            velocity = 1*(min_dist - 1)
-            #clip velocity to 0 and 0.5
-            velocity = max(0, min(1, velocity))
-            self.publish_vel([velocity, 0])
-
 class FetchMove:
     """Move fetch with velocity commands"""
     def __init__(self):
-        self.laser_scan_subscriber = rospy.Subscriber('base_scan', LaserScan, self.laser_scan_callback)
+        self.laser_scan_subscriber = rospy.Subscriber('base_scan_filtered', LaserScan, self.laser_scan_callback)
         self.vel_controller = VelocityController()
         self.size = 1
-        self.tf_listener = tf.TransformListener()
-        self.cartesian_in_front = None
-        self.laser_msg = None
-    
+        
     def laser_scan_callback(self, msg):
-        self.laser_msg = msg
-        ranges_cartesian = self.polar_to_cartesian(msg.ranges, msg.angle_min, msg.angle_max, msg.angle_increment)
-        #filter ranges_cartesian if it is in front
-        self.cartesian_in_front = list(filter(self.is_point_in_front, ranges_cartesian))
-        self.move()
-
-    def polar_to_cartesian(self, ranges: List[float], angle_min: float, angle_max: float, angle_increment: float):
-        angle = np.arange(angle_min, angle_max, angle_increment)
-        x = ranges * np.cos(angle)
-        y = ranges * np.sin(angle)
-        return list(zip(x, y))
-
-    def is_point_in_front(self, range):
-        fetch_points = [0., 0.]#get transform from world to laser
-        #Will need robot orientation to get the correct min and max
-
-        x_min = fetch_points[0] - 0.5
-        x_max = fetch_points[0] + 0.5
-        y_min = fetch_points[1]
-        y_max = fetch_points[1] + 1
-        x = range[0]
-        y = range[1]
-        return x_min < x < x_max and y_min < y < y_max
+        self.move(msg.ranges)
     
-    def move(self):
-        self.vel_controller.forward_polar(self.laser_msg.ranges)
+    def move(self, ranges):
+        self.vel_controller.forward_polar(ranges=ranges)
 
 
 
---
+++ src/rob599/rob599_hw1/src/laserscan_filter.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+import rospy
+import sys
+from geometry_msgs.msg import LaserScan
+import numpy as np
+from typing import List
+import copy
+
+class LaserScanFilter:
+    """Take in raw laser scan and filter to points just in front of fetch"""
+    def __init__(self):
+        self.laser_scan_subscriber = rospy.Subscriber('base_scan', LaserScan, self.filter_callback)
+        self.laser_scan_publisher = rospy.Publisher('base_scan_filtered', LaserScan)
+        
+    def filter_callback(self, msg):
+        filtered_msg = copy.deepcopy(msg)
+        ranges_cartesian = self.polar_to_cartesian(msg.ranges, msg.angle_min, msg.angle_max, msg.angle_increment)
+        polar_filtered_ranges = list(map(self.is_point_in_front, ranges_cartesian))
+        filtered_msg.ranges = polar_filtered_ranges
+        self.laser_scan_publisher.publish(filtered_msg)
+        
+    @staticmethod
+    def polar_to_cartesian(ranges: List[float], angle_min: float, angle_max: float, angle_increment: float):
+            angle = np.arange(angle_min, angle_max, angle_increment)
+            x = ranges * np.cos(angle)
+            y = ranges * np.sin(angle)
+            return list(zip(x, y))
+
+
+    def is_point_in_front(self, range):
+        fetch_points = [0., 0.]#get transform from world to laser
+        #Will need robot orientation to get the correct min and max
+
+        x_min = fetch_points[0] - 0.5
+        x_max = fetch_points[0] + 0.5
+        y_min = fetch_points[1]
+        y_max = fetch_points[1] + 1
+        x = range[0]
+        y = range[1]
+        if x_min < x < x_max and y_min < y < y_max:
+            return np.sqrt(np.square(x)+np.square(y))
+        else:
+            return np.inf
+
+if __name__ == '__main__':
+    # Initialize the node.
+    rospy.init_node('laserscan_filter')
+    laserscan_filter = LaserScanFilter()
+    rospy.spin()
